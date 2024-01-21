6610d5f716629ec21a5025a620a4fa188af59dd6
austinCreed <sudhansu.sekhar.jena.24@gmail.com>
2024-01-21 13:06:37 +0530

    first commit

---
+++ item.py
@@ -0,0 +1,31 @@
+items = [
+        {
+            "tags": [
+                "python",
+                "python-3.x",
+                "pyside2",
+                "3dsmax",
+                "shiboken2"
+            ],
+            "owner": {
+                "account_id": 18468091,
+                "reputation": 17,
+                "user_id": 13453722,
+                "user_type": "registered",
+                "profile_image": "https://lh5.googleusercontent.com/-KFnu5QQeD44/AAAAAAAAAAI/AAAAAAAAAAA/AAKWJJNJAYVWPYH_Vkm1H4SYbracN8IXPw/photo.jpg?sz=256",
+                "display_name": "Lasse",
+                "link": "https://stackoverflow.com/users/13453722/lasse"
+            },
+            "is_answered": False,
+            "view_count": 2,
+            "answer_count": 0,
+            "score": 0,
+            "last_activity_date": 1705687756,
+            "creation_date": 1705687756,
+            "question_id": 77848034,
+            "content_license": "CC BY-SA 4.0",
+            "link": "https://stackoverflow.com/questions/77848034/importerror-dll-load-failed-while-importing-qtmaxnative",
+            "title": "ImportError: DLL load failed while importing QtMaxNative"
+        },
+    ]
+
